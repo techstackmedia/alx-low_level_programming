2af0fbbaedd6d3b38dee15bd4376d120fb296097
Bello Osagie <osagienoah04@gmail.com>
2023-06-20 18:21:41 +0000

    add a file file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - Print _putchar
+ * main - Entry point
+ *
  * Return: Always 0
  */
 int main(void)
@@ -15,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
