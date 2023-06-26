df815c039685094a57141ac87561f3d323baf45f
Bello Osagie <osagienoah04@gmail.com>
2023-06-26 23:48:25 +0000

    pointer array string
    Write a function that convert a string to an integer.
    
    Prototype: int _atoi(char *s);
    The number in the string can be preceded by an infinite number of characters
    You need to take into account all the - and + signs before the number
    If there are no numbers in the string, the function must return 0
    You are not allowed to use long
    You are not allowed to declare new variables of “type” array
    You are not allowed to hard-code special values
    We will use the -fsanitize=signed-integer-overflow gcc flag to compile your code.
    FYI: The standard library provides a similar function: atoi. Run man atoi to learn more.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,40 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer
- * @s: Pointer to the string
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
  *
- * Return: The converted integer
+ * Return: A integer
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int num = 0;
-	int started = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	while (*s != '\0')
+	while (s[c])
 	{
-		if (*s == '-')
-			sign *= -1;
-
-		if (*s >= '0' && *s <= '9')
+		if (s[c] == 45)
 		{
-			started = 1;
-			num = num * 10 + (*s - '0');
+			min *= -1;
 		}
-		else if (started)
+
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (isi == 1)
 		{
 			break;
 		}
 
-		s++;
+		c++;
 	}
 
-	return num * sign;
+	ni *= min;
+	return (ni);
 }
-
